8b218ac2a3a7f4c8f511f95dc7acbfb17f6ea112
Rishab Jain <jainrishabbohra@gmail.com>
2022-09-30 22:38:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ let guest_score = 0
 setHS = document.getElementById('home-score')
 setGS = document.getElementById('guest-score')
 
+let prevEle = document.getElementById('prev')
+prevEle.innerText = `Previous Score : Home (${localStorage.getItem('HprevScore')}) Guest (${localStorage.getItem('GprevScore')})`
+
+
 function HP(score){
   home_score += score
   setHS.innerText = home_score
@@ -17,8 +21,8 @@ function GP(score){
 
 function winner(){
   if(home_score>guest_score){
-    document.getElementById('team1').innerText = "Home 🥳"
-    document.getElementById('team2').innerText = "Guest 😭"
+    document.getElementById('team1').innerText = "Home 👑"
+    document.getElementById('team2').innerText = "Guest"
   }
   
   else if (home_score==guest_score){
@@ -27,20 +31,19 @@ function winner(){
   }
 
   else {
-    document.getElementById('team2').innerText = "Guest 🥳"
-    document.getElementById('team1').innerText = "Home 😭"
+    document.getElementById('team2').innerText = "Guest 👑"
+    document.getElementById('team1').innerText = "Home"
   } 
 }
 
 function reset(){
-  localStorage.setItem("scores", JSON.stringify([[home_score, guest_score]]))
-  console.log(JSON.parse(localStorage.getItem("scores")))
-  let prevScore = document.getElementById("prev")
-  prevScore.innerText = `Previous Scores : Home (${home_score}) Guest (${guest_score})`
+  localStorage.setItem('HprevScore', home_score)
+  localStorage.setItem('GprevScore', guest_score)
+  prevEle.innerText = `Previous Score : Home (${localStorage.getItem('HprevScore')}) Guest (${localStorage.getItem('GprevScore')})`
   home_score = 0
   guest_score = 0
   setHS.innerText = home_score
   setGS.innerText = guest_score
-  document.getElementById('team2').innerText = "Guest  "
-  document.getElementById('team1').innerText = "Home  "
+  document.getElementById('team2').innerText = "Guest"
+  document.getElementById('team1').innerText = "Home"
 }
